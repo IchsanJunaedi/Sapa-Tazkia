6e5892dd0c912124c809daf013d65daaec51fe99
ican <brotherhome15@gmail.com>
2026-04-01 15:44:23 +0700

    test(config): extend jest to cover integration tests with 30s timeout

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,9 +1,13 @@
 // backend/jest.config.js
 module.exports = {
   testEnvironment: 'node',
-  testMatch: ['**/tests/**/*.test.js', '**/src/__tests__/**/*.test.js'],
+  testMatch: [
+    '**/tests/unit/**/*.test.js',
+    '**/tests/integration/**/*.test.js',
+    '**/src/__tests__/**/*.test.js',
+  ],
   setupFiles: ['./tests/setup.js'],
-  testTimeout: 10000,
+  testTimeout: 30000,
   verbose: true,
   moduleNameMapper: {
     '^pdf-parse$': '<rootDir>/tests/__mocks__/pdf-parse.js',
